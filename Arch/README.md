6a64f394e80ab31648b57b4dc7fbc546f4fd9b8e
Vamsi Kolati <vamsikolati.99@gmail.com>
2021-02-18 12:32:53 +0530

    Rebuilt the directory with a new kali attackbox, nothing changed from the arch attackbox

---
+++ Arch/README.md
@@ -0,0 +1,4 @@
+## ArchAttackBox
++ ArchAttackBox is my custom docker image.
++ It is built with Arch linux as base OS with default shell as fish.
++ It comes with all the tools I need pre-installed.
---
+++ Arch/configure.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+#Setting local time
+ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+
+# Setting dotfiles variable
+dotfiles="/tmp/dotfiles"
+mkdir /root/.config
+cp $dotfiles/.tmux.conf /root/.tmux.conf
+cp -r $dotfiles/.config/fish /root/.config/fish
+
+
+# Installing spacevim
+echo -e "[+]${BGreen}Installing SpaceVim${Color_Off}"
+curl -sLf https://spacevim.org/install.sh | bash
+
+
+# Installing OhMyFish
+echo -e "[+]${BGreen}Installing SpaceVim${Color_Off}"
+curl -L https://get.oh-my.fish | fish
+fish -c "omf install neolambda"
